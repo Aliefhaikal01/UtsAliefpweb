ca49ac4e70851befbdec6c707b108c4f4bfcbae5
Alif <aliefhaikal01@gmail.com>
2022-05-12 21:25:04 +0700

    uts siap

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,7 +8,7 @@ export default {
 <template>
       <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
-          <a class="navbar-brand"  href="/logo"><img src="../assets/navbarquran.png" class="rounded-circle" width="30" height="30"  ></a>
+          <a class="navbar-brand"  to="/logo"><img src="../assets/navbarquran.png" class="rounded-circle" width="30" height="30"  ></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
